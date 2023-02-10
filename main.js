61d9e43000c5863be8520e90af87cda6a77ba2f0
Florian Wentzel <flolow2005@gmail.com>
2023-02-10 02:01:27 +0100

    images added

--- main.js
+++ main.js
@@ -6,17 +6,15 @@ async function main() {
     var typDropDown;
 
     const data = await LoadJsonData();//LoadData();
-    
+
     //this is final never chancing
 
-    console.log("jonas ------")
-    console.log(data.hersteller)
-    console.log(data.models)
-    console.log(data.types)
     herstellerDropDown = CreateSelectionMenu("herstellerDiv", data.hersteller);
     modelDropDown = CreateSelectionMenu("modelDiv", data.models[0]);
     typDropDown = CreateSelectionMenu("typDiv", data.types["00"]);
-
+    
+    setPicture(data.getImagesByHerstellerAndModel(herstellerDropDown.value, modelDropDown.value));
+    
     herstellerDropDown.addEventListener('change', (event) => {
         RemoveAllOptions(modelDropDown);
         RemoveAllOptions(typDropDown);
@@ -32,7 +30,7 @@ async function main() {
             AddOptionToSelect(typDropDown, element);
         });
 
-        setPicture(data.getImagesByHerstellerAndModel[hersteller, modelDropDown.value]);
+        setPicture(data.getImagesByHerstellerAndModel(hersteller, modelDropDown.value));
 
 
     });
@@ -46,7 +44,7 @@ async function main() {
             AddOptionToSelect(typDropDown, element);
         });
 
-        setPicture(data.getImagesByHerstellerAndModel[hersteller, model]);
+        setPicture(data.getImagesByHerstellerAndModel(hersteller, model));
     });
 
     typDropDown.addEventListener('change', (event) => {
@@ -74,9 +72,9 @@ async function LoadJsonData() {
                 // var val = json["hersteller"][keys[i]] [Object.keys(json["hersteller"][keys[i]])] ["name"];
                 models[i.toString()].push(modelArr[j]["name"]);
                 images[i.toString()].push(modelArr[j]["image"]);
-                types[i.toString()+j.toString()] = [];
+                types[i.toString() + j.toString()] = [];
                 for (var z = 0; z < modelArr[j]["types"].length; z++) {
-                    types[i.toString()+j.toString()].push(modelArr[j]["types"][z]);
+                    types[i.toString() + j.toString()].push(modelArr[j]["types"][z]);
                 }
             }
         }
@@ -105,7 +103,12 @@ async function LoadJsonData() {
             getImagesByHerstellerAndModel: function (herstellerName, modelName) {
                 const herstellerIndex = hersteller.indexOf(herstellerName);
                 const modelIndex = models[herstellerIndex].indexOf(modelName);
-                const type = images[herstellerIndex.toString() + modelIndex.toString()];
+                const type = images[herstellerIndex.toString()][modelIndex.toString()];
+                console.log("getImage");
+                console.log(herstellerIndex);
+                console.log(modelIndex);
+                console.log(type);
+
                 return type;
             }
         }
@@ -115,75 +118,6 @@ async function LoadJsonData() {
         alert("Fehler beim laden der datei.");
     }
 }
-
-function LoadData() {
-    var data = readStringFromFilePath("test.txt").split(' ').join('').split("\r").join("");
-    // console.log(text);
-    var lines = data.split("\n");
-    if (lines.length <= 0) {
-        alert("Fehler beim laden der Datei!");
-    }
-    //erste Zeile sind die hersteller
-
-    //List<String>
-    var hersteller = lines[0].split(",");
-
-    //Map<Integer, String>
-    var models = {};//index vom hersteller
-
-    //Map<String, String>
-    var types = {};//string bestehend aus nummern welche z.B. "01" für Hersteller index 0 und model index 1 
-
-    //um typ 3 von model 2 von hersteller 1 zu bekommen:
-
-    //beginne bei der zweiten Zeile
-    for (var i = 0; i < hersteller.length; i++) {
-        models[i] = [];
-        for (var j = 1; j < lines.length; j++) {
-            try {
-                var modelsWithTypData = lines[j].split(",")[i];
-                if (modelsWithTypData == "") continue;
-                //get type Data
-                var modelNameWithDataList = modelsWithTypData.split("*");
-                var modelName = modelNameWithDataList.splice(0, 1);
-                models[i].push(modelName[0]);
-                // console.log(modelName);
-                types[i.toString() + (j - 1).toString()] = modelNameWithDataList;
-            } catch (err) {
-                console.log(err);
-            }
-        }
-    }
-
-    /** ÄNDERUNG
-     * Hier habe ich ein Objekt erstellt, das hersteller, models, types speichert damit man diese Daten returnen kann.
-     * Zudem sind 2 Funktionen enthalten, diese werden für die unteren zeilen benötigt (eventListener)
-    */
-
-    const returnObject = {
-        hersteller: hersteller,
-        models: models,
-        types: types,
-        getModelsByHersteller: function (herstellerName) {
-            const herstellerIndex = hersteller.indexOf(herstellerName);
-            const herstellerModels = models[herstellerIndex];
-            return herstellerModels;
-        },
-        getTypeByHerstellerAndModel: function (herstellerName, modelName) {
-            const herstellerIndex = hersteller.indexOf(herstellerName);
-            const modelIndex = models[herstellerIndex].indexOf(modelName);
-            const type = types[herstellerIndex.toString() + modelIndex.toString()];
-            // console.log(types)
-            // console.log(modelIndex)
-            // console.log(hersteller)
-            return type;
-        }
-    }
-
-    return returnObject;
-}
-
-
 
 function CreateSelectionMenu(divName, array) {
     var whereToPut = document.getElementById(divName);
