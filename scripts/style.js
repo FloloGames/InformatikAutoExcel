7ae7b665d64d511cf66a546dc5e7f5a207acdb35
Jonas <58705742+JxstJonas@users.noreply.github.com>
2023-02-14 22:30:56 +0100

    feat: Add displayed content to radio

--- scripts/style.js
+++ scripts/style.js
@@ -2,17 +2,26 @@ const selections = document.querySelectorAll('.labelInfo');
 var activeItem = document.querySelector(".active");
 const selection__Box = document.querySelector('.selection__box');
 const info = document.querySelector(".info");
+const cards = document.querySelectorAll('.cards');
 
 
 
-function onClick (item, index) {
-    if(activeItem == item) return;
+function onClick(item, index) {
+    if (activeItem == item) return;
 
-    if(activeItem) {
-        activeItem.classList.remove('active');
-    }
+    selections.forEach((element, number) => {
+        element.classList.remove('active');
+    })
 
-    console.log(item)
+    const value = item.getElementsByTagName('input').info.value;
+
+    const box = document.querySelector("." + value);
+    cards.forEach((element, index_) => {
+        if(element.classList.contains('flex')) {
+            element.classList.remove('flex');
+        }
+    });
+    box.classList.add('flex');
 
     activeItem.classList.add('active')
     activeItem = item;
@@ -25,11 +34,8 @@ selections.forEach((selection, index) => {
 
 
 function offsetMenuBorder(element, menuBorder) {
-
-    console.log(element)
-    const left = Math.floor(element.offsetLeft - info.offsetLeft - (selection__Box.offsetWidth  - element.offsetWidth) / 2) +  "px";
+    const left = Math.floor(element.offsetLeft - info.offsetLeft - (selection__Box.offsetWidth - element.offsetWidth) / 2) + "px";
     selection__Box.style.transform = `translate3d(${left}, 0 , 0)`;
-
 }
 
 
